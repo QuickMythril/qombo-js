8112837d35b51791d72eec22975b13a525f51e57
QuickMythril <quickmythril@protonmail.com>
2024-01-28 17:00:39 -0500

    Added Apps

---
+++ apps.js
@@ -0,0 +1,103 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetchBlockHeight()
+        .then(() => {
+            searchByName('');
+        })
+        .catch(error => {
+            console.error('An error occurred in the fetch chain:', error);
+        });
+});
+
+document.getElementById('search-button').addEventListener('click', handleSearch);
+document.getElementById('search-input').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        handleSearch();
+    }
+});
+
+function fetchBlockHeight() {
+    return fetch('/blocks/height')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('block-height').textContent = data;
+            return data;
+        })
+        .catch(error => {
+            document.getElementById('block-height').textContent = `Error fetching block height: ${error}`;
+            console.error('Error fetching block height:', error);
+        });
+}
+
+function handleSearch() {
+    const searchQuery = document.getElementById('search-input').value;
+    if (!searchQuery) return;
+    if (searchQuery.startsWith('Q') && !searchQuery.includes('0') && !searchQuery.includes('O') && !searchQuery.includes('I') && !searchQuery.includes('l') && searchQuery.length >= 26 && searchQuery.length <= 35) {
+        validateAddress(searchQuery);
+    } else if (searchQuery.length >= 0 && searchQuery.length <= 40) {
+        searchByName(searchQuery);
+    }
+}
+
+function validateAddress(address) {
+    fetch('/addresses/validate/' + address)
+        .then(response => response.json())
+        .then(isValid => {
+            if (isValid) {
+                fetchAddressDetails(address);
+            } else {
+                searchByName(address);
+            }
+        })
+        .catch(error => console.error('Error validating address:', error));
+}
+
+function fetchAddressDetails(address) {
+    Promise.all([
+        fetch('/names/address/' + address).then(response => response.json())
+    ]).then(([names]) => {
+        if (names.length > 0) {
+            searchByName(names[0].name);
+        } else {
+            searchByName(address);
+        }
+    }).catch(error => console.error('Error fetching address details:', error));
+}
+
+function searchByName(name) {
+    document.getElementById('app-results').innerHTML = '';
+    fetch('/arbitrary/resources/search?service=APP&name=' + name)
+    .then(response => response.json())
+    .then(results => {
+        if (results.length > 0) {
+            let tableHtml = '<table>';
+            tableHtml += `
+                <tr>
+                    <th>Name</th>
+                    <th>Size</th>
+                    <th>Created</th>
+                    <th>Updated</th>
+                </tr>
+            `;
+            results.forEach(result => {
+                tableHtml += `
+                    <tr>
+                        <td>${result.name}</td>
+                        <td>${result.size}</td>
+                        <td>${new Date(result.created).toLocaleString()}</td>
+                        <td>${new Date(result.updated).toLocaleString()}</td>
+                    </tr>
+                `;
+            });
+            tableHtml += '</table>';
+            document.getElementById('app-results').innerHTML = tableHtml;
+            const exactMatch = results.find(r => r.name.toLowerCase() === name.toLowerCase());
+            if (exactMatch) {
+                // TODO - Display specific app info.
+                // fetchAddressDetails(exactMatch.owner);
+            }
+        } else {
+            document.getElementById('app-results').innerHTML = '<p>No results found.</p>';
+        }
+    })
+    .catch(error => console.error('Error searching by name:', error));
+}
--- blocks.js
+++ blocks.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     fetchBlockHeight(fetchAndDisplayBlocks);
+    fetchBlockHeight(searchByBlock);
+});
+
+document.getElementById('search-button').addEventListener('click', handleSearch);
+document.getElementById('search-input').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        handleSearch();
+    }
 });
 
 function fetchBlockHeight(callback) {
@@ -35,6 +43,85 @@ function fetchAndDisplayBlocks(height) {
             });
         })
         .catch(error => console.error('Error fetching blocks:', error));
+}
+
+function handleSearch() {
+    const currentBlockHeight = parseInt(document.getElementById('block-height').textContent);
+    const searchQuery = document.getElementById('search-input').value;
+    if (!searchQuery) return;
+    if (+searchQuery >= 1 && +searchQuery <= +currentBlockHeight) {
+        searchByBlock(+searchQuery);
+    } else {
+        document.getElementById('block-details').innerHTML = `<p>Invalid search: ${searchQuery}</p>`;
+        return;
+    }
+}
+
+function searchByBlock(height) {
+    document.getElementById('block-details').innerHTML = '';
+    fetch('/blocks/byheight/' + height)
+        .then(response => response.json())
+        .then(result => {
+            if (result) {
+                let resultHtml = '';
+                result => {
+                    let shortenedSignature = result.signature.substring(0, 4) + '...' + result.signature.substring(result.signature.length - 4);
+                    resultHtml += `
+                        <p>${result.height} - ${shortenedSignature} - ${result.transactionCount} Txs - ${result.onlineAccountsCount} Minters - ${new Date(result.timestamp).toLocaleString()}</p>
+                    `;
+                    if (result.transactionCount > 0) {
+                        fetchAndDisplayTxs(result.signature);
+                    }
+                };
+                document.getElementById('block-details').innerHTML = resultHtml;
+            } else {
+                document.getElementById('block-details').innerHTML = `<p>Block ${height} not found.</p>`;
+            }
+        })
+        .catch(error => {
+            document.getElementById('block-details').innerHTML = `<p>Error searching by block: ${error}</p>`;
+            console.error('Error searching by block:', error);
+        });
+}
+
+async function fetchAndDisplayTxs(signature) {
+    try {
+        const response = await fetch(`/transactions/block/${signature}`);
+        const txs = await response.json();
+        const tableBody = document.getElementById('txs-table').getElementsByTagName('tbody')[0];
+        txs.sort((a, b) => b.timestamp - a.timestamp);
+        txs.forEach(async tx => {
+            let row = document.createElement('tr');
+            row.insertCell(0).textContent = tx.blockHeight;
+            let shortenedSignature = tx.signature.substring(0, 4) + '...' + tx.signature.substring(tx.signature.length - 4);
+            row.insertCell(1).textContent = shortenedSignature;
+            row.insertCell(2).textContent = tx.type;
+            let nameOrAddress = await displayNameOrAddress(tx.creatorAddress);
+            row.insertCell(3).textContent = nameOrAddress;
+            row.insertCell(4).textContent = tx.fee;
+            let formattedTimestamp = formatTimestamp(tx.timestamp);
+            row.insertCell(5).textContent = formattedTimestamp;                
+            tableBody.appendChild(row);
+        });
+    } catch (error) {
+        console.error('Error fetching transactions:', error);
+    }
+}
+
+async function displayNameOrAddress(address) {
+    let shortenedAddress = address.substring(0, 4) + '...' + address.substring(address.length - 4);
+    try {
+        const response = await fetch(`/names/address/${address}`);
+        const names = await response.json();
+        if (names[0]) {
+            return `${names[0].name}`;
+        } else {
+            return `(${shortenedAddress})`;
+        }
+    } catch (error) {
+        console.error('Error fetching name:', error);
+        return `(${shortenedAddress})`;
+    }
 }
 
 document.getElementById('load-more').addEventListener('click', function() {
